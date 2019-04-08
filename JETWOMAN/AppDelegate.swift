acb67ef549c3e891bdb2482d9f7f0e76808b90df
richard <richardp.leandro@gmail.com>
2019-04-08 14:42:16 -0300

    Initial Commit

---
+++ JETWOMAN/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  JETWOMAN
+//
+//  Created by richardleandro on 08/04/19.
+//  Copyright © 2019 richardleandro. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
